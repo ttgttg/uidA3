de619cb3e9078e5d0d8baba34646d0c6733cdab2
Jiayi Hu <alicejiayihu@hotmail.com>
2024-05-28 16:38:00 +1000

    changed naming conventions

--- js/indextest.js
+++ js/indextest.js
@@ -1,11 +1,11 @@
 //move object while mouse is hovering
 document.addEventListener("mousemove", parallax);
 function parallax(e) {
-  document.querySelectorAll(".graphicObject").forEach((graphicObject) => {
-    let moveValue = graphicObject.getAttribute("data-value");
+  document.querySelectorAll(".graphicShape").forEach((graphicShape) => {
+    let moveValue = graphicShape.getAttribute("data-value");
     let x = (e.clientX * moveValue) / 200;
     let y = (e.clientY * moveValue) / 200;
 
-    graphicObject.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    graphicShape.style.transform = `translateX(${x}px) translateY(${y}px)`;
   });
 }
