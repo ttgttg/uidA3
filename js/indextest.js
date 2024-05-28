10fb6865a901c9f702515a1d351fbe70a8054728
Jiayi Hu <alicejiayihu@hotmail.com>
2024-05-29 01:02:44 +1000

    slideshow

--- js/indextest.js
+++ js/indextest.js
@@ -1,3 +1,21 @@
+// Get the navbar
+let navbar = document.querySelector(".nav");
+
+// Get the offset position of the navbar
+let sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyNav() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// When the user scrolls the page, execute myFunction
+window.addEventListener("scroll", stickyNav);
+
 //move object while mouse is hovering
 document.addEventListener("mousemove", parallax);
 function parallax(e) {
