5a5b9ba41875d7c4e5a7fa580ca64d45379c654b
isabelletowndrow <itowndrow@student.unimelb.edu.au>
2024-05-31 18:24:55 +1000

    shopping cart

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const cartIcon = document.getElementById('cartIcon');
-  const cart = document.getElementById('cart');
-
-  // Toggle the 'show' class on the cart when the cart icon is clicked
-  cartIcon.addEventListener('click', function() {
-    cart.classList.toggle('show');
+    const cartIcon = document.getElementById('cartIcon');
+    const cart = document.getElementById('cart');
+    const continueShopping = document.querySelector('.continueShopping');
+  
+    // Toggle the 'show' class on the cart when the cart icon is clicked
+    cartIcon.addEventListener('click', function() {
+      cart.classList.toggle('show');
+    });
+  
+    // Close the cart when "Continue Shopping" text is clicked
+    continueShopping.addEventListener('click', function() {
+      cart.classList.remove('show');
+    });
   });
-});
-
+  
