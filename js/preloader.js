8ed0eee05553cdebaad91c597f65060e4e3e67ae
Jiayi Hu <alicejiayihu@hotmail.com>
2024-06-02 11:28:51 +1000

    added preloader

---
+++ js/preloader.js
@@ -0,0 +1,10 @@
+const preloader = document.querySelector("#preloader");
+if (preloader) {
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      preloader.remove();
+      window.addEventListener("scroll", handleAlert);
+      handleAlert();
+    }, 2500);
+  });
+}
