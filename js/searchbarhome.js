5f7ed176c092c6a2acf316b61538599c2279574e
Jiayi Hu <alicejiayihu@hotmail.com>
2024-06-03 13:40:56 +1000

    corrected prdouct list path from homepage

---
+++ js/searchbarhome.js
@@ -0,0 +1,30 @@
+const icon = document.querySelector(".nav__search__icon");
+const search = document.querySelector(".nav__search");
+const clearButton = document.querySelector(".nav__search__clear");
+icon.onclick = function loadSearch() {
+  search.classList.toggle("active");
+};
+
+//If the user presses Enter and the input is not empty, navigate to the product page.
+//If the user presses Enter and the input is empty, display an alert.
+const input = document.getElementById("mySearch");
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    if (input.value.trim() !== "") {
+      window.location.assign("html/product-list.html");
+    } else {
+      alert("Please enter something to start browsing!");
+    }
+  }
+});
+
+//If the user clicks the clear button and the input is empty, display an alert.
+//If the user clicks the clear button and the input is not empty, clear the input field.
+clearButton.addEventListener("click", function () {
+  if (input.value.trim() === "") {
+    alert("Please enter something to clear input!");
+  } else {
+    input.value = ""; // Clear the input field if it is not empty
+  }
+});
