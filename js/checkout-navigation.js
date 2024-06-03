987b7828481ce4be5ace9f2dcca79cda95dc9be8
Jiayi Hu <alicejiayihu@hotmail.com>
2024-06-03 15:44:47 +1000

    fixed file links

--- js/checkout-navigation.js
+++ js/checkout-navigation.js
@@ -2,5 +2,5 @@
 checkoutBtn.addEventListener('click', () => {
     console.log('Checkout button clicked'); // Check if this message appears in the browser console
     // Redirect the user to the checkout page
-    window.location.href = '/html/checkout.html'; // Replace 'checkout-page.html' with the actual URL of your checkout page
+    window.location.href = "../html/checkout.html"; // Replace 'checkout-page.html' with the actual URL of your checkout page
 });
