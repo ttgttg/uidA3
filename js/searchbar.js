d191d788f95a3e08f53b82fe6aaa11c54c777a93
Jiayi Hu <alicejiayihu@hotmail.com>
2024-06-03 11:53:47 +1000

    fuxed shopping cart & search bar

--- js/searchbar.js
+++ js/searchbar.js
@@ -5,7 +5,6 @@ icon.onclick = function loadSearch() {
   search.classList.toggle("active");
 };
 
-
 //If the user presses Enter and the input is not empty, navigate to the product page.
 //If the user presses Enter and the input is empty, display an alert.
 const input = document.getElementById("mySearch");
@@ -13,13 +12,12 @@ input.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
     if (input.value.trim() !== "") {
-      window.location.assign("product-list.html");
+      window.location.assign("html/product-list.html");
     } else {
       alert("Please enter something to start browsing!");
     }
   }
 });
-
 
 //If the user clicks the clear button and the input is empty, display an alert.
 //If the user clicks the clear button and the input is not empty, clear the input field.
